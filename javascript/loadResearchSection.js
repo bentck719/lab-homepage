308263ec5164f1a15efa28dd6a05d78c911bce4b
Galafala <ben100719@gmail.com>
2024-11-18 00:58:15 +0800

    Feat: Add a new class called research-topic-box for styling

--- javascript/loadResearchSection.js
+++ javascript/loadResearchSection.js
@@ -11,8 +11,7 @@ function createResearchSection(title, content) {
 		"p-2",
 		"px-4",
 		"text-white",
-		"bg-primary",
-		"bg-opacity-70"
+		"research-topic-box"
 	);
 	sectionTitle.textContent = title;
 
