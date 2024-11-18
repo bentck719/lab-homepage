8f3cef8d213ab9b00f08400a74593eab434a9ccb
Galafala <ben100719@gmail.com>
2024-11-18 11:52:56 +0800

    Feat: Add animation to Navbar
    - Independent Navbar to a html file
    - Add nav-items class to replace nav-item

--- javascript/loadFooter.js
+++ javascript/loadFooter.js
@@ -3,4 +3,4 @@ async function loadFooter() {
 	const content = await footer.text();
 	document.getElementById("footer-container").innerHTML = content;
 }
-window.onload = loadFooter;
+window.addEventListener("DOMContentLoaded", loadFooter);
---
+++ javascript/loadNavbar.js
@@ -0,0 +1,6 @@
+async function loadNavbar() {
+	const footer = await fetch("/public/components/navbar.html");
+	const content = await footer.text();
+	document.getElementById("navbar-container").innerHTML = content;
+}
+window.addEventListener("DOMContentLoaded", loadNavbar);
