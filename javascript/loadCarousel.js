16e001fdddb7a92612de9b822134fe94dfcdf9be
Galafala <ben100719@gmail.com>
2024-11-18 16:12:39 +0800

    Fix: Update file path

--- javascript/loadCarousel.js
+++ javascript/loadCarousel.js
@@ -1,5 +1,5 @@
 async function loadCarousel() {
-	const carousel = await fetch("public/components/carousel.html");
+	const carousel = await fetch("/public/components/carousel.html");
 	const content = await carousel.text();
 	document.getElementById("carousel-container").innerHTML = content;
 }
--- javascript/loadNavbar.js
+++ javascript/loadNavbar.js
@@ -1,5 +1,5 @@
 async function loadNavbar() {
-	const footer = await fetch("public/components/navbar.html");
+	const footer = await fetch("/public/components/navbar.html");
 	const content = await footer.text();
 	document.getElementById("navbar-container").innerHTML = content;
 }
